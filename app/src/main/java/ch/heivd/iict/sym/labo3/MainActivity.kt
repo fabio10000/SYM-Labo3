01ec8e5120e79b4423d89f7fad637f46ad9c7dce
RosalieChhen <mei-heang.chhen@heig-vd.ch>
2021-12-07 09:21:34 +0100

    3 buttons

---
+++ app/src/main/java/ch/heivd/iict/sym/labo3/MainActivity.kt
@@ -0,0 +1,21 @@
+package ch.heivd.iict.sym.labo3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var barCode_btn: Button
+    private lateinit var beacon_btn : Button
+    private lateinit var nfc_btn: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        barCode_btn = findViewById(R.id.main_barCode_bt)
+        beacon_btn = findViewById(R.id.main_beacon_bt)
+        nfc_btn = findViewById(R.id.main_nfc_bt)
+    }
+}
