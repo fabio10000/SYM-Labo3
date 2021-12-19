6d1281ea0f1952bc0ada6e16f9013ff45c09774b
Fabio Marques <fabiomanuelsilvamarques@gmail.com>
2021-12-19 12:16:29 +0100

    Login page

--- app/src/main/java/ch/heigvd/iict/sym/labo3/MainActivity.kt
+++ app/src/main/java/ch/heigvd/iict/sym/labo3/MainActivity.kt
@@ -1,5 +1,6 @@
 package ch.heigvd.iict.sym.labo3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -17,5 +18,10 @@ class MainActivity : AppCompatActivity() {
         barCode_btn = findViewById(R.id.main_barCode_bt)
         beacon_btn = findViewById(R.id.main_beacon_bt)
         nfc_btn = findViewById(R.id.main_nfc_bt)
+
+        nfc_btn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
